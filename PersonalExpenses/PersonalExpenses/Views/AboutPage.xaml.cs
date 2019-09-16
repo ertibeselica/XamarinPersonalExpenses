57f125e3514118f5676a5adfb04968e21a28458d
ebeselica <erti.beselica@asseco-see.com>
2019-09-16 20:45:53 +0200

    Add project files.

---
+++ PersonalExpenses/PersonalExpenses/Views/AboutPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PersonalExpenses.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
